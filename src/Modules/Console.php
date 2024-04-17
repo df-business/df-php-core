dd4996461e6f9bedba8c8caca3ece2475cdefeac
dfer <df_business@qq.com>
2024-04-17 14:44:42 +0800

    1.4.4

--- src/Modules/Console.php
+++ src/Modules/Console.php
@@ -108,7 +108,7 @@ class Console extends Common
     {
       $projectRootDir = ROOT;
       // 模块项目所在的目录，非开发者无法使用该功能
-      $moduleRootDir = dirname(ROOT) . DIRECTORY_SEPARATOR . ' ' . DIRECTORY_SEPARATOR . 'root' . DIRECTORY_SEPARATOR;
+      $moduleRootDir = dirname(ROOT) . DIRECTORY_SEPARATOR . 'df-php-root' . DIRECTORY_SEPARATOR . 'root' . DIRECTORY_SEPARATOR;
 
       // 需要同步的目录
       $dir = [
